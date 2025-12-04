97549cb931b0f47ac7793ffc4d529013c4847a8b
jose luis <carrerasoto53@gmail.com>
2025-12-04 10:11:59 -0600

    config: Increase Angular bundle size limits and enable Tailwind CSS JIT mode with purge.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit", // Enable JIT mode for smaller builds
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts,css,scss,sass,less}"],
+    // Safelist classes that might be generated dynamically
+    safelist: [],
+  },
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
